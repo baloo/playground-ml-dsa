56b96db2c4bb8540df5fcbd82a56ce01b52d826b
Arthur Gautier <arthur.gautier@arista.com>
2025-11-10 19:17:43 -0800

    bump dependencies, use ML-DSA-87

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 use std::{ops::Deref, str::FromStr, time::Duration};
 
-use ml_dsa::{KeyPair, MlDsa44};
+use ml_dsa::{KeyPair, MlDsa87};
 use pkcs8::{DecodePrivateKey, EncodePrivateKey};
 use signature::Keypair;
 use x509_cert::{
-    builder::{profile, Builder, CertificateBuilder},
-    der::{pem::LineEnding, EncodePem},
+    SubjectPublicKeyInfo,
+    builder::{Builder, CertificateBuilder, profile},
+    der::{EncodePem, pem::LineEnding},
     name::Name,
     serial_number::SerialNumber,
     time::Validity,
-    SubjectPublicKeyInfo,
 };
 
-static PRIVATE_KEY: &str = include_str!("./ML-DSA-44.priv");
+static PRIVATE_KEY: &str = include_str!("./ML-DSA-87-seed.priv");
 
 fn main() {
-    let keypair = KeyPair::<MlDsa44>::from_pkcs8_pem(PRIVATE_KEY).expect("parse private key");
+    let keypair = KeyPair::<MlDsa87>::from_pkcs8_pem(PRIVATE_KEY).expect("parse private key");
 
-    let pub_key = SubjectPublicKeyInfo::from_key(&keypair.verifying_key()).unwrap();
+    let pub_key = SubjectPublicKeyInfo::from_key(keypair.verifying_key()).unwrap();
 
-    let serial_number = SerialNumber::generate(&mut rand::thread_rng()).unwrap();
+    let serial_number = SerialNumber::generate(&mut rand::thread_rng());
     let validity = Validity::from_now(Duration::new(5, 0)).unwrap();
     let subject = Name::from_str("CN=hi,O=Acme Inc,C=US").unwrap();
     let profile = profile::cabf::Root::new(false, subject).expect("Create root profile");
